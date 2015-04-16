f192baa44a7ad19a97df21f97c21ebc86e244782
avao <dev@avao.co>
2015-04-16 17:56:15 +0100

    assert for serialisation test

--- Src/BasicModel_CS_Test/SerialisationTest.cs
+++ Src/BasicModel_CS_Test/SerialisationTest.cs
@@ -8,22 +8,30 @@ using Types.rootNs;
 using System.Xml;
 using BasicModel.CS.Serialisation;
 using Serialisers;
+using Codge.TestSystem;
+using Codge.TestSystem.FileBased;
 
 namespace BasicModel_CS_Test
 {
     
     public class SerialisationTest
     {
+        public static TestSystem TestSystem = new TestSystem(new DataStorage("../../TestStore/Serialisation"));
+
         [Test]
         public void SerialiseToXml()
         {
-            var obj = new myType2(2, true, new int[]{4});
+            var obj = new myType2(2, true, new int[]{4,5});
             var serialiser = new Serialisers.rootNs.myType2();
+
+            var testCase = TestSystem.GetTestCase("Xml");
+
 
             SerialisationContext context = new SerialisationContext();
             Registrar.RegisterSerialisers(context);
 
-            string v = Utils.Serialise(obj, "root", context);
+            string actualContent = Utils.Serialise(obj, "root", context);
+            testCase.AssertContentXml(actualContent, "serialised.xml", true);
         }
     }
 }
