ab2b69b192c41e9f20aad97e9107b36646e859aa
avao <dev@avao.co>
2015-10-17 07:47:01 +0100

    Qart update. ModelXsd UT

--- Src/Codge.Generator.Test/XsdLoader.cs
+++ Src/Codge.Generator.Test/XsdLoader.cs
@@ -20,22 +20,6 @@ namespace Codge.Generator.Test
     {
         public static TestSystem TestSystem = new TestSystem(new DataStore("../../TestStore/XsdLoader"));
 
-        [Test]
-        public void LoadModelXsd()
-        {
-            var testCase = TestSystem.GetTestCase("LoadModelXsd");
-
-            ModelDescriptor modelDescriptor;
-            var schema = SchemaLoader.Load(@"..\..\..\Codge.Generator\Presentations\Xml\Model.xsd");
-            modelDescriptor = ModelLoader.Load(schema, "MyModel");
-
-            var compiler = new ModelCompiler();
-            var typeSystem = new TypeSystem();
-            var model = compiler.Compile(typeSystem, modelDescriptor);
-
-            testCase.AssertContentXml(TypeSystemXmlSerialiser.ToString(typeSystem), "TypeSystem.xml", true);
-        }
-
         [TestCase("LoadXsd")]
         [TestCase("SequenceInChoice")]
         [TestCase("BuiltInTypes")]
@@ -44,8 +28,9 @@ namespace Codge.Generator.Test
         [TestCase("Enumeration")]
         [TestCase("ElementWithEmbededType")]
         [TestCase("Group")]
+        [TestCase("ModelXsd")]
         public void Process(string testId)
-            {
+        {
             var testCase = TestSystem.GetTestCase(testId);
 
             XmlSchema schema = testCase.UsingReadStream("Model.xsd", stream => SchemaLoader.Load(stream));
