7adc8c18a16c405924f4e84fc9427cbcbbdd78c1
avao <dev@avao.co>
2015-12-01 17:33:00 +0000

    PathMapper moved to a separate file

--- Src/Codge.Generator/IOutputPathMapper.cs
+++ Src/Codge.Generator/IOutputPathMapper.cs
@@ -1,47 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Codge.Generator
 {
     public interface IOutputPathMapper
     {
         string MapPath(ItemInformation itemInfo, Context context);
-    }
-
-    public class OutpuPathMapper : IOutputPathMapper
-    {
-        public string MapPath(ItemInformation itemInfo, Context context)
-        {
-            string path = itemInfo.Item;
-            if(string.IsNullOrEmpty(itemInfo.Category))
-            {
-                return path;
-            }
-            path = path.Replace('.', Path.DirectorySeparatorChar);
-
-            string extension = null;
-            string category = itemInfo.Category;
-            int pos = itemInfo.Category.LastIndexOf('.');
-            if (pos != -1)
-            {
-                extension = itemInfo.Category.Substring(pos);
-                category = itemInfo.Category.Substring(0, pos);
-            }
-
-            if(!string.IsNullOrEmpty(category))
-            {
-                path = Path.Combine(category, path);
-            }
-            if(extension != null)
-            {
-                path += extension;
-            }
-
-            return path;
-        }
     }
 }
